a83583b3d87434fefc5be9891f9192d48be1af75
ArvishLandis <dauzzzg@gmail.com>
2020-05-14 02:11:03 +0800

    get the main jsonobj in mainAct

--- WeatherApp/app/src/main/java/com/example/wheaterapp/Https.java
+++ WeatherApp/app/src/main/java/com/example/wheaterapp/Https.java
@@ -1,9 +1,18 @@
 package com.example.wheaterapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
@@ -13,24 +22,74 @@ import java.security.cert.CertStore;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
+import java.util.Iterator;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 
-public class Https {
+public class Https extends AsyncTask<Void,Void,JSONObject>{
     private String weatherURL = "https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-A0010-001?Authorization=CWB-D1C038E5-7E4E-4A75-A32D-6E42A1B835C1&downloadType=WEB&format=JSON";
-    //private String
-    private URL url;
+
+    private JSONObject jsonObject;
+    private BufferedReader bufferedReader;
     private HttpsURLConnection httpsConnection;
 
+    @SuppressLint("StaticFieldLeak")
     private Context context;
 
-    public Https(Context context){
+    Https(Context context){
         this.context = context;
     }
 
+    private void setHttpsConnection(){
+        try {
+            //private String
+            URL url = new URL(weatherURL);
+            httpsConnection = (HttpsURLConnection) url.openConnection(); //not establish a connection with the server, need to call connect()
+            httpsConnection.setSSLSocketFactory((SSLSocketFactory) SSLSocketFactory.getDefault());
+            httpsConnection.setDoInput(true);
+            httpsConnection.connect();
+            bufferedReader = new BufferedReader(new InputStreamReader(httpsConnection.getInputStream())); // if it's too long maybe I would change it to more readable way.
+
+            StringBuilder stringBuffer = new StringBuilder(); //first I use stringbuffer, but the gradle recommended me to use stringbuilder(not thread safe)
+            String line = "";
+            while((line = bufferedReader.readLine())!= null){
+                stringBuffer.append(line);// don't need \n
+               //Log.i("httpGetData",line);
+            }
+            //Log.i("httpString",stringBuffer.toString());
+            jsonObject = new JSONObject(stringBuffer.toString());
+            //int in = jsonObject.length();
+            //Log.i("httpsThread","length "+in);
+            //Log.i("httpThread",jsonObject.getJSONObject("北部地區").toString());
+            //Iterator<String> iterator = jsonObject.keys();
+            /*while(iterator.hasNext()){
+                String key = iterator.next();
+                //Log.i("httpsThread","key: "+key);
+                //Log.i("httpsThread","value: "+jsonObject.getString(key));
+            }*/
+            //Log.i("httpsThread","out while");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(bufferedReader != null)
+                try{
+                    bufferedReader.close();
+                    httpsConnection.disconnect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    public JSONObject getJsonObject(){
+        return jsonObject;
+    }
+    //maybe don't need this if the default ca could do all the work
     private SSLContext setSSLContext(){ //manual, auto --> SSLContextFactory.getDefault();
         try{
             InputStream inputStream = context.getAssets().open("");
@@ -51,5 +110,11 @@ public class Https {
             e.printStackTrace();
         }
         return null;
+    }
+
+    @Override
+    protected JSONObject doInBackground(Void... voids) {
+        setHttpsConnection();
+        return jsonObject;
     }
 }
--- WeatherApp/app/src/main/java/com/example/wheaterapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/wheaterapp/MainActivity.java
@@ -1,20 +1,61 @@
 package com.example.wheaterapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.icu.text.Edits;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {
+                    Manifest.permission.INTERNET
+            },200);
+        }
+        Https https = new Https(getApplicationContext());
+        List<JSONObject> myJSONList = new ArrayList<>();
+        try {
+            JSONObject jsonObject = https.execute().get();
+            JSONObject cwbdata = jsonObject.getJSONObject("cwbdata");
+            JSONObject resources = cwbdata.getJSONObject("resources");
+            JSONObject resource = resources.getJSONObject("resource");
+            JSONObject data = resource.getJSONObject("data");
+            JSONObject agrWeather = data.getJSONObject("agrWeatherForecasts");
+            JSONObject weatherForecasts = agrWeather.getJSONObject("weatherForecasts");
+            JSONArray location = weatherForecasts.getJSONArray("location");
+            HashMap<String,JSONObject> map = new HashMap<>();
+            for(int i=0;i<location.length();i++){
+                myJSONList.add(location.getJSONObject(i));
+                Log.i("mainThread",location.getString(i));
+            }
+        } catch (ExecutionException | InterruptedException | JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
